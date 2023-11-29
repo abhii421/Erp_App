87b56d87f8fcc2db4e65951f893188249f5112e4
Gaurav Yadav <Gaurav88689548@gmail.com>
2023-11-30 00:49:30 +0530

    add payment ui

--- erp_app2/lib/subject.dart
+++ erp_app2/lib/subject.dart
@@ -89,21 +89,24 @@ class SubjectScreen extends StatelessWidget {
               ),
             ),
           ),
-          Padding(
-            padding: const EdgeInsets.all(16.0),
-            child: Column(
-              crossAxisAlignment: CrossAxisAlignment.center,
-              
-              children: [
+          Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            
+            children: [
 
-                Padding(
-                  padding: const EdgeInsets.only(left: 45, top: 130, bottom: 50),
-                  child: Text('Subject: $subject', style: TextStyle( color: Colors.white, fontSize: 27, fontWeight: FontWeight.bold),),
-                ),
-                Text('      Total Present: ${totalPresent ?? 'N/A'}' , style: TextStyle( color: Colors.white, fontSize: 17, fontWeight: FontWeight.normal),),
-                Text('     Total Absent: ${totalAbsent ?? 'N/A'}' , style: TextStyle( color: Colors.white, fontSize: 17, fontWeight: FontWeight.normal),),
-              ],
-            ),
+              Padding(
+                padding: const EdgeInsets.only(top: 140, left: 40, bottom: 60),
+                child: Text('Subject: $subject', style: TextStyle( color: Color.fromARGB(255, 255, 255, 255), fontSize: 25, fontWeight: FontWeight.bold),),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 30),
+                child: Text('Total Present: ${totalPresent ?? 'N/A'}' , style: TextStyle( color: Color.fromARGB(255, 255, 255, 255), fontSize: 17, fontWeight: FontWeight.normal),),
+              ),
+              Padding(
+                padding: const EdgeInsets.only(left: 30),
+                child: Text('Total Absent: ${totalAbsent ?? 'N/A'}' , style: TextStyle( color: Color.fromARGB(255, 255, 255, 255), fontSize: 17, fontWeight: FontWeight.normal),),
+              ),
+            ],
           ),
         ],
       ),
